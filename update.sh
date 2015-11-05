564e40414eb809a9a51b1b713c8b064c39da8fd1
Benjamin Fleischer <github@benjaminfleischer.com>
2015-11-04 20:00:07 -0600

    Update feed

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash --login
 git pull --rebase origin master && \
     ruby extract_episodes.rb && \
-    ruby build_episodes.rb  && \
+    ruby build_episodes.rb
     git add episodes/ && \
-    git commit -am 'Update feed' && \
+    git commit -am 'Update feed'
     git push origin && \
     git push heroku
